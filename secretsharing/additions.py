94afdc5da3e4af77c96358499ac000d2d04df6b5
Shantanu Rane <srane@parc.com>
2015-10-08 00:11:26 -0700

    Initial support for negative numbers, more checking needed.
    Must use modulo notation for negative answers

--- secretsharing/additions.py
+++ secretsharing/additions.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-    Secret Sharing
-    ~~~~~
-
-    :copyright: (c) 2014 by Halfmoon Labs
-    :license: MIT, see LICENSE for more details.
+    Additive Homomorphic Property of Shamir Secret Sharing
 """
 
 import string
@@ -15,12 +11,12 @@ from polynomials import random_polynomial, \
     get_polynomial_points, modular_lagrange_interpolation
 from sharing import secret_int_to_points_given_prime, points_to_secret_int
 
-secret1 = 123L
-secret2 = 556L
+secret1 = 123
+secret2 = -556
 num_points = 7
 threshold = 3
 
-prime = get_large_enough_prime([max(secret1, secret2), num_points])
+prime = get_large_enough_prime([max(abs(secret1), abs(secret2)), num_points])
 
 shares1 = secret_int_to_points_given_prime(secret1, threshold, num_points, prime)
 print shares1
@@ -32,4 +28,9 @@ for i in range(1,num_points+1):
 	shares3.append((i, (shares1[i-1][1] + shares2[i-1][1]) % prime))
 print shares3
 
-print points_to_secret_int(shares3[1:7])
+recovered_sum = points_to_secret_int(shares3[1:7])
+
+if recovered_sum > prime/2:
+	recovered_sum -= prime
+
+print 'Recovered Sum is', recovered_sum
